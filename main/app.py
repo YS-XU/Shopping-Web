2dbc024f53bd10799ae1549af3d8667cf85e3396
Yaosheng <xysss.jason@gmail.com>
2020-07-18 10:41:58 -0400

    flask

--- main/app.py
+++ main/app.py
@@ -1,1 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.run()
