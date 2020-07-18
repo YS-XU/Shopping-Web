463cdda0a6a01b94ee9369a288d87c036674b7a3
Jimmytran16 <jimmytran1620@gmail.com>
2020-07-18 12:25:49 -0400

    created a base html

--- main/app.py
+++ main/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
 
-
 app = Flask(__name__)
 mysql = MySQL(app) 
 
---
+++ main/static/js/register.js
@@ -0,0 +1,38 @@
+// Get the elements in the document
+var error_span_msg = document.getElementById('password-error-container'); //Get the span div for error messsage
+var register_btn = document.getElementById('register-btn'); //register button
+var register_container = document.getElementById('register-container'); //register container
+var login_container = document.getElementById('login-container') // login container
+var register_view_button = document.getElementById('register-view-btn'); //register view button
+var login_view_button = document.getElementById('login-view-btn') //login view button
+showLogin(); //Show login first
+hideSpan(); // Hide the error message span
+
+function checkIfMatch(){
+  if(document.getElementById('password1').value !== document.getElementById('password2').value){ //if both password fields dont match then ... display the error message, disable the register button
+      error_span_msg.style.display = '';
+      register_btn.disabled = true;
+  }
+  else{
+    error_span_msg.style.display = 'none'; //else enable the register button and hide the error message
+    register_btn.disabled = false;
+  }
+}
+
+function hideSpan(){ //Function to hide the span
+  error_span_msg.style.display = 'none';
+}
+
+function showLogin() { //disable register and enable log in, change font sizes -- change bottom border
+  register_container.style.display = 'none';
+  login_container.style.display = '';
+  login_view_button.style.borderBottom = "3px solid black";
+  register_view_button.style.borderBottom = "1px solid grey"
+}
+
+function showRegister() { //disable login  and enable register -- change the bottom border
+  register_container.style.display = '';
+  login_container.style.display = 'none';
+  register_view_button.style.borderBottom = "3px solid black";
+  login_view_button.style.borderBottom = "1px solid grey"
+}
