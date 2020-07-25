04e5845d09fc90acf4aba1328ac8f7b5f2020451
Jimmytran16 <jimmytran1620@gmail.com>
2020-07-25 11:05:14 -0400

    userhome add

--- main/app.py
+++ main/app.py
@@ -1,30 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,session
 from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv,find_dotenv
 
 app = Flask(__name__)
-mysql = MySQL(app) 
+mysql = MySQL(app)
+
+load_dotenv(find_dotenv('.env')) #finds the .env file
 
 #set up the database configurations
-app.config['MYSQL_HOST'] = 'account.cdtpomlk2gyj.us-east-2.rds.amazonaws.com'
-app.config['MYSQL_USER'] = 'admin'
-app.config['MYSQL_PASSWORD'] = '$hopping1234'
-app.config['MYSQL_DB'] = 'shoppingwebapp'
-app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_HOST'] = os.getenv('HOST')
+app.config['MYSQL_USER'] = os.getenv('USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('PASS')
+app.config['MYSQL_DB'] = os.getenv('DB')
+app.config['MYSQL_PORT'] = int(os.getenv('PORT'))
 
 @app.route('/getdata') #route to test the database
 def data():
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM testinguser;')
+    cur.execute('SELECT * FROM testinguser;') #get all the data from the user table
     rv = cur.fetchall()
     return str(rv)
 
-@app.route("/")
+@app.route("/") #route to the home page
 def home():
-    return render_template("index.html")
+    exist = None
+    if session['user'] is not None:
+        exist = session['user']
+    return render_template("index.html",user=exist)
 
-@app.route("/register/")
+@app.route("/userhome/") #route to the home page
+def userhome():
+    return render_template("userhome.html")
+
+
+@app.route("/register/") #route to the register page
 def register():
+    if session['user'] is not None:
+        return redirect('userhome')
     return render_template("register.html")
+
+@app.route("/login/",methods=['POST']) #route to the register page
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        #query the database for user table by the username
+        #select * from user where username = 'username'
+        #if user does exist, then compare the password
+
+        #if the user exist and password matches then...
+        session['user'] = username
+
+        print(username,password)
+
+
+        return redirect('userhome',name=name)
+
+
+
+
 
 
 if __name__ == "__main__":
