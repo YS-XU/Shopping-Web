3167d6382b5f5f63b9f7702d196d8065005f720a
Jimmytran16 <jimmytran1620@gmail.com>
2020-07-29 02:04:17 -0400

    created personal details page

--- main/app.py
+++ main/app.py
@@ -20,14 +20,15 @@ app.config['MYSQL_PORT'] = int(os.getenv('PORT'))
 def data():
     con = mysql.connection
     cursor = con.cursor()
-    
+
     #cursor.execute("INSERT INTO testinguser (firstname, lastname) VALUES ('yaosheng', 'xu');")
     #con.commit()
     cursor.execute('SELECT * FROM testinguser;') #get all the data from the user table
     rv = cursor.fetchall()
-    
+
     return str(rv)
 
+@app.route('/home')
 @app.route("/") #route to the home page
 def home():
     exist = None
@@ -35,11 +36,17 @@ def home():
         session['user'] = None
     if session['user'] is not None:
         exist = session['user']
+        print(exist)
     return render_template("index.html",user=exist)
 
 @app.route("/userhome/") #route to the home page
 def userhome():
-    return render_template("user/account.html")
+    #get the user object from the session
+    if 'user' not in session or session['user'] == None: #if the user is not logged in then they don't have access to this page
+        return redirect('/')
+    else: #if user does exist, get the user's firstname from the session
+        user = session.get('user')
+    return render_template("user/account.html",user=user)
 
 @app.route("/register/") #route to the register page
 def register():
@@ -56,11 +63,11 @@ def signup():
         firstname = request.form.get('Firstname')
         lastname = request.form.get('Lastname')
         setPassword = request.form.get('Passwords')
-        
+
         #Check if the user exist
         if cursor.execute("SELECT * FROM USER WHERE Email LIKE %s", [email]):
             return render_template("error.html")
-        
+
         #insert new user information into user table
         cursor.execute("INSERT INTO USER (Firstname, Lastname, Email, Passwords) VALUES (%s, %s, %s, %s)",
         (firstname, lastname, email, setPassword))
@@ -83,10 +90,14 @@ def login():
                 #if the user exist and password matches then login succuess
                 session['user'] = cursor.execute("SELECT Firstname FROM USER WHERE Email = %s", [username])
                 return redirect('/userhome/')
-       
+
         return render_template("error.html")
 
-         
+
+@app.route('/personal/') #route to the user's personal settings page
+def personal_details():
+    return render_template('user/personaldetails.html')
+
 
 if __name__ == "__main__":
     app.config['SECRET_KEY'] = 'fdsfsdfdsfsdfdsfsfsdf'
